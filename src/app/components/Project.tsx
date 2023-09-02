b941ec4281b149ee71a9e4ae9ae3599bf5d4bf31
RONALD BRAYAN CASTRO TORRES <ronaldcastrobt@unimagdalena.edu.co>
2023-09-02 13:45:03 -0500

    projectOptions-style

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -172,7 +172,7 @@ const Project: React.FC<projectProps> = ({ proyecto, indice, indiceMax }) => {
             </CardContent>
             <CardActions disableSpacing>
 
-                <Stack direction="row" gap={0.5} justifyContent="center" alignItems="center">
+                <Stack direction="row" gap={0.7} justifyContent="center" alignItems="center">
                     <Checkbox {...label} sx={{ border: borderProperties, borderRadius: 6, p: 0.5 }}
                         icon={<FavoriteBorderIcon sx={{ color: "#FFFFFF" }} />}
                         onChange={handleLike} checked={checkFavorite}
