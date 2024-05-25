c7adb5cf07a1580ae240265935787c7b1d35a2aa
Ronald Castro <ronaldcastro1512@gmail.com>
2024-05-25 12:49:56 -0500

    gap - footer appbar - 25/05/2024

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -30,7 +30,7 @@ const Footer = () => {
     return (
         <AppBar position="fixed" sx={{ top: "auto", bottom: 0, width: "100%", 
                 backgroundColor: "#ffffff" }}>
-            <Toolbar sx={{ display: "flex", flex: "1 1 auto", justifyContent: "center", gap: 5 }}>
+            <Toolbar sx={{ display: "flex", justifyContent: "center", gap: 3 }}>
                 <IconButton color="default" aria-label="home button">
                     <HomeIcon sx={{ color: "#000000" }} />
                 </IconButton>
